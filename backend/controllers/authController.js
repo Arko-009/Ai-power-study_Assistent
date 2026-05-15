b7e9331d7b3bbafc9f15ff460658dde824ade637
arkobag558-creator <arkobag558@gmail.com>
2026-05-15 19:28:34 +0530

    Timestamp - 37:00

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,6 +4,43 @@ import User from "../models/User.js";
 //genrate JWT token 
 const genrateToken = (id) =>{
     return jwt.sign({id},process.env.JWT_SECRET,{
-        
+        expiresIn: process.env.JWT_EXPIRE || "7d",
     });
+};
+
+// @desc Register 
+// @route POST /api/auth/register
+// @access public
+
+export const register = async (req,res,next) => {
+    try{
+        
+    }catch(error){
+       next(error);
+    }
+}
+
+// @desc Login
+// @route POST /api/auth/login
+// @access public
+export const login = async(req,res,next) =>{
+
+}
+// @desc User profile
+// @route GET /api/auth/profile
+// @access private
+export const getProfile = async(req,res,next) =>{
+
+}
+// @desc Update User profile
+// @route PUT /api/auth/profile
+// @access private
+export const updateProfile = async(req,res,next) =>{
+
+}
+// @desc change password
+// @route POST /api/auth/chnage-password
+// @access private
+export const changePassword = async(req,res,next) =>{
+
 }
